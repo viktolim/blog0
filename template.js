12447229e4e671afe139af9ecced7f2c9226de84
viktolim <83975054+viktolim@users.noreply.github.com>
2022-09-03 11:35:41 +0300

    Add files via upload

---
+++ template.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Template = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default Template;
